d0a0aa45cd438166443cb25d989d9590ed008dd0
Josh <9366888+hollanjs@users.noreply.github.com>
2024-09-16 11:34:29 -0400

    Update FauxMyPosh_AllInOne.ps1

--- FauxMyPosh_AllInOne.ps1
+++ FauxMyPosh_AllInOne.ps1
@@ -652,20 +652,7 @@ $gitBadge     = New-FMPBlock @themedGitBadgeConfig
 $promptSymbol = New-FMPBlock @themedPromptSymbolConfig
 
 
-<# Below is a cmdlet that holds the logic for how the prompt should be written to the cli
-    TO DO:
-        [ ] make class that handles 'packing' fmpblocks into prompt
-        [ ] class should pack into lines
-        [ ] should provide interface to set row and heirarchy for packing
-        [ ] should allow for multiple rows for fmpblocks per prompt
-        [ ] should allow right and left alignment with empty between
-        [ ] class should also have method to chain call updates of fmpblocks  
-        [ ] implement some sort of basic fallback functionality for multiple refresh script
-            [ ] refresh scripts should have heirarchy
-            [ ] fallbacks should check fmpblock if important
-                [ ] if fmpblock not important, and there are no more fallback refreshscripts,
-                    remove block based on heirarchy as cli width becomes increasingly smaller
-#>
+<# Below is a cmdlet that holds the logic for how the prompt should be written to the cli #>
 function Use-FauxPrompt {
     param()
 
